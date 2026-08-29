89ec1162e49170e3066369e3ae73b5d780bac686
123Farz <farzeenpatel2011@gmail.com>
2026-08-29 14:36:14 +0300

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,3 @@
+num = int(input("What number do you choose"))
+for i in range(1,11):
+    print(f"{num} x {i} = {num * i}")
---
+++ act3.py
@@ -0,0 +1,8 @@
+totalsum = 0
+num = 1
+
+while num <= 14:
+    totalsum += num
+    num +=1
+
+print(f"The sume of the first 14 natural numbers is {totalsum}")
